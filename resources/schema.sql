a7e64695aa76d18391b384dd09346c3b7450f481
Sven Reissmann <sven@0x80.io>
2013-09-25 00:04:16 +0200

    updated database schema, added version, readme and setup

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,35 +1,43 @@
 -- Copyright 2013 Dustin Frisch<fooker@lab.sh>
 -- 
--- This file is part of ddns.
+-- This file is part of ddserver.
 -- 
--- ddns is free software: you can redistribute it and/or modify
+-- ddserver is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
 -- 
--- ddns is distributed in the hope that it will be useful,
+-- ddserver is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
 -- 
 -- You should have received a copy of the GNU General Public License
--- along with ddns.  If not, see <http://www.gnu.org/licenses/>.
+-- along with ddserver.  If not, see <http://www.gnu.org/licenses/>.
 
 
 DROP TABLE IF EXISTS `hosts`;
 DROP TABLE IF EXISTS `users`;
 
 
+--
+-- table users
+--
 CREATE TABLE `users` (
   `id`          INT            NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `username`    VARCHAR(255)   NOT NULL UNIQUE,
   `password`    VARCHAR(255)   NOT NULL,
   `email`       VARCHAR(255)   NOT NULL,
+  `admin`       BOOLEAN        NOT NULL DEFAULT FALSE,
   `created`     TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
   
   INDEX (`username`)
-);
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
 
+
+--
+-- table hosts
+--
 CREATE TABLE `hosts` (
   `id`          INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_id`     INT             NOT NULL,
@@ -43,16 +51,17 @@ CREATE TABLE `hosts` (
   INDEX (`address`),
   
   INDEX (`user_id`, `hostname`)
-);
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
+
+ALTER TABLE `hosts`
+  ADD CONSTRAINT `hosts_ibfk_2` 
+  FOREIGN KEY (`user_id`) 
+  REFERENCES `users` (`id`) 
+  ON DELETE CASCADE;
 
 
-INSERT INTO `users` VALUES (1, 'major',  '$6$rounds=61868$ix.et15etZ5iCo3i$RzjlA9Sg8OczEZesjARktmFPICiLWEx91Gx9sziNHWBwZKWQ.txgDtCjYX1xDf/UEnKc6/VT2GHWAa3FEmSIz0', 'foo', NOW()),
-                           (2, 'fooker', '$6$rounds=60024$Fo5CiSSxZ6.8ImPd$nxmZpbnEUtLiAsMfOaTqQROfskLPYF65asHgqBlo9ErlknBUfI7WN6gbo3kj8QUGun9jfczDBZ5WMXY0YXLYT0', 'foo', NOW());
-
-INSERT INTO `hosts` VALUES (1, 2, 'fooker', NULL, NOW()),
-                           (2, 2, 'home.fooker', NULL, NOW()),
-                           (3, 2, 'mom.fooker', NULL, NOW()),
-                           (4, 2, 'fooker1', NULL, NOW()),
-                           (5, 2, 'fooker2', NULL, NOW()),
-                           (6, 1, 'a', NULL, NOW()),
-                           (7, 1, 'b', NULL, NOW());
+--
+-- default user admin with password admin
+--
+INSERT INTO `users` 
+  VALUES (1, 'admin', '$6$rounds=65488$jZYVZUGK9mNdQeTj$2MTmn67qFtmg.xKbonwE4OcwCe9z64duk0vgh3nCP6yIBiERxk3t4hFYuytxZF0jmwbpSyq.B3DKtb6CyQ2tG.', 'admin@example.com', 0, NOW());
---
+++ setup.py
@@ -0,0 +1,59 @@
+'''
+Copyright 2013 Sven Reissmann <sven@0x80.io>
+
+This file is part of ddserver.
+
+ddserver is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ddserver is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ddserver. If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import setuptools
+
+
+version = open('./VERSION').read().strip()
+
+setuptools.setup(
+    license = 'GNU GPLv3',
+
+    name = 'ddserver',
+    version = version,
+
+    author = 'Sven Reissmann',
+    author_email = 'sven@0x80.io',
+
+    url = 'http://dev.open-desk.net/projects/ddserver',
+
+    description = 'A server-side application for dynamic DNS management.',
+    long_description = open('README').read(),
+    keywords = 'ddserver ddns',
+
+    install_requires = [
+        'beaker >= 1.6',
+        'bottle-mysql >= 0.1',
+        'bottle >= 0.11',
+        'jinja2 >= 2.6',
+        'formencode >= 1.2',
+        'passlib >= 1.6'
+    ],
+
+    data_files = [
+        ('/etc/ddserver', ['resources/config/settings.conf']),
+        ('/usr/share/ddserver', ['resources/web', 'resources/schema.sql']),
+    ],
+
+    entry_points = {
+        'console_scripts' : [
+            'ddserver = ddserver.__main__:main',
+        ]
+    },
+)
