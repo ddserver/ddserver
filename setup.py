acb47efd31757dc8d36e68bad018c6cf81b4f18a
Sven Reissmann <sven@0x80.io>
2013-10-22 18:20:42 +0200

    switched to the GNU Affero General Public License Version 3

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ Copyright 2013 Sven Reissmann <sven@0x80.io>
 This file is part of ddserver.
 
 ddserver is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
 ddserver is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
+You should have received a copy of the GNU Affero General Public License
 along with ddserver. If not, see <http://www.gnu.org/licenses/>.
 '''
 
