75adb8c900d3183606bcca3f6323459092c35dc4
Sven Reissmann <sven@0x80.io>
2017-11-12 18:55:59 +0100

    update setup.py, PEP8ify, update project URL, limit mysql-connector version to be <2.1.5, to prevent brotobuf dependency issue (maybe not the best solution - needs to be verified). Closes #27.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-'''
-Copyright 2013 Sven Reissmann <sven@0x80.io>
+"""
+Copyright 2017 Sven Reissmann <sven@0x80.io>
 
 This file is part of ddserver.
 
@@ -15,7 +15,7 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with ddserver. If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
 import setuptools
 
@@ -23,27 +23,27 @@ import setuptools
 version = open('./VERSION').read().strip()
 
 setuptools.setup(
-    license = 'GNU AGPLv3',
+    license='GNU AGPLv3',
 
-    name = 'ddserver',
-    version = version,
+    name='ddserver',
+    version=version,
 
-    author = 'Sven Reissmann',
-    author_email = 'sven@0x80.io',
+    author='Sven Reissmann',
+    author_email='sven@0x80.io',
 
-    url = 'https://ddserver.0x80.io',
+    url='https://ddserver.org',
 
-    description = 'A server-side application for dynamic DNS management.',
-    long_description = open('README.md').read(),
-    keywords = 'ddserver ddns dns nsupdate',
+    description='A server-side application for dynamic DNS management.',
+    long_description=open('README.md').read(),
+    keywords='ddserver ddns dns nsupdate',
 
-    install_requires = [
+    install_requires=[
         'beaker >= 1.6',
         'bottle >= 0.11',
         'jinja2 >= 2.6',
         'formencode >= 1.3.0a1',
         'passlib >= 1.6',
-        'mysql-connector >= 2.0.0',
+        'mysql-connector >= 2.0.0, < 2.1.5',
         'require >= 0.1.0',
         'requests >= 2.5.0',
         'enum34 >= 1.0.0',
@@ -52,11 +52,11 @@ setuptools.setup(
         'Pdns-Remotebackend >= 0.6'
     ],
 
-    packages = setuptools.find_packages(),
+    packages=setuptools.find_packages(),
 
-    zip_safe = False,
+    zip_safe=False,
 
-    package_data = {
+    package_data={
         'ddserver.resources': [
             'email/*.mail',
             'web/css/*.css',
@@ -65,9 +65,9 @@ setuptools.setup(
             'templates/*.html',
         ],
     },
-    include_package_data = True,
+    include_package_data=True,
 
-    data_files = [
+    data_files=[
         ('doc', ['README.md', 'INSTALL.md', 'VERSION', 'LICENSE',
                  'CHANGES.md', 'UPGRADING.md']),
         ('doc/conf', ['ddserver/resources/doc/ddserver.conf.example',
@@ -82,7 +82,7 @@ setuptools.setup(
                       'ddserver/resources/doc/httpd.conf'])
     ],
 
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'ddserver-interface = ddserver.interface.__main__:main',
             'ddserver-updater = ddserver.updater.__main__:main',
